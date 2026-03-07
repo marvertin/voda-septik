d19f3f1aa9f9db3c654aac0e4c874147c381cbec
Martin Veverka <martin.veverka@email.cz>
2026-03-07 12:00:09 +0100

    Piny pro teploty

--- firmware/main/pins.h
+++ firmware/main/pins.h
@@ -13,9 +13,9 @@ static const gpio_num_t TM1637_DIO_GPIO = GPIO_NUM_19;          // GPIO19
 static const gpio_num_t I2C_SDA_GPIO = GPIO_NUM_21;             // GPIO21
 static const gpio_num_t I2C_SCL_GPIO = GPIO_NUM_22;             // GPIO22
 
-static const gpio_num_t FLOW_SENSOR_GPIO = GPIO_NUM_17;         // GPIO17
+static const gpio_num_t FLOW_SENSOR_GPIO = GPIO_NUM_26;         // GPIO26
 static const gpio_num_t BOOT_BUTTON_GPIO = GPIO_NUM_0;          // GPIO0
-static const gpio_num_t TEMPERATURE_SENSOR_GPIO = GPIO_NUM_16;  // GPIO16
+static const gpio_num_t TEMPERATURE_SENSOR_GPIO = GPIO_NUM_27;  // GPIO27
 
 static const adc_channel_t LEVEL_SENSOR_ADC_CHANNEL = ADC_CHANNEL_6;          // ADC1_CH6 (GPIO34)
 
