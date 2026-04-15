0b38566a60ce89efbbd671125874131ab55b25b3
Martin Veverka <martin.veverka@email.cz>
2026-04-15 20:47:39 +0200

    Změna názvu pkonstanty pinů úrp display, prootže přijdou i jiné I2C

--- firmware/main/pins.h
+++ firmware/main/pins.h
@@ -10,8 +10,8 @@ static const gpio_num_t AUX_LED_GPIO = GPIO_NUM_4;              // GPIO4
 static const gpio_num_t TM1637_CLK_GPIO = GPIO_NUM_18;          // GPIO18
 static const gpio_num_t TM1637_DIO_GPIO = GPIO_NUM_19;          // GPIO19
 
-static const gpio_num_t I2C_SDA_GPIO = GPIO_NUM_22;             // GPIO22
-static const gpio_num_t I2C_SCL_GPIO = GPIO_NUM_21;             // GPIO21
+static const gpio_num_t DISPLAY_I2C_SDA_GPIO = GPIO_NUM_22;             // GPIO22
+static const gpio_num_t DISPLAY_I2C_SCL_GPIO = GPIO_NUM_21;             // GPIO21
 
 static const gpio_num_t FLOW_SENSOR_GPIO = GPIO_NUM_26;         // GPIO26
 static const gpio_num_t BOOT_BUTTON_GPIO = GPIO_NUM_0;          // GPIO0
